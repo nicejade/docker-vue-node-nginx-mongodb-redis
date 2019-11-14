c1b07d7ffdff4245adcfbed3dfc1a0f4fd9af61c
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2019-11-14 23:57:19 +0800

    :fire: remove vue client

--- client/src/helper/index.js
+++
@@ -1,8 +0,0 @@
-/** @format */
-
-export const $ajax = require('./ajax').default
-export const $apis = require('./apis').default
-export const $auth = require('./auth').default
-export const $utils = require('./utils').default
-export const $lodash = require('./lodash').default
-export const $document = require('./document').default
